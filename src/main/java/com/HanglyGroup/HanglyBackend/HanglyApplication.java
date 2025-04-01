8212ec60d6320c069db5183e71f6888c2bbd9c46
valadao-davi <davi.raichu@gmail.com>
2025-04-01 11:30:57 -0300

    initial commit and environtment settings

---
+++ src/main/java/com/HanglyGroup/HanglyBackend/HanglyApplication.java
@@ -0,0 +1,13 @@
+package com.HanglyGroup.HanglyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanglyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanglyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HanglyGroup/HanglyBackend/HanglyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HanglyGroup.HanglyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanglyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
