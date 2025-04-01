b462e88af38c1e632911809edc19ff0a15432ad8
valadao-davi <davi.raichu@gmail.com>
2025-04-01 11:40:58 -0300

    feat: user entity

---
+++ src/main/java/com/HanglyGroup/HanglyBackend/entities/User.java
@@ -0,0 +1,56 @@
+package com.HanglyGroup.HanglyBackend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_tab")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+    private String email;
+    private String password;
+
+    public User(){}
+
+    public User(Long userId, String name, String email, String password){
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
